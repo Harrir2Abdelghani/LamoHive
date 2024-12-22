eb170bfb0c9ea54002fe08565db6c2c7830aa705
HARRIR HABIB ABDELGHANI <100070886+Harrir2Abdelghani@users.noreply.github.com>
2024-12-22 18:10:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
       <Sidebar onAdd={handleAdd}/> 
       <div className='flex-1 p-6 '>
           <h1 className='text-3xl font-bold mb-6'>LamoHive Dashboard</h1>
+    <!-- Intentlead -->
+<div style="width: 100%">
+    <script src="https://app.intentlead.com/js/wAeZ3I55ax"></script>
+</div>
           <button onClick={() => setPopupOpen(true)} className='bg-yellow-300 text-white px-4 py-2 rounded-lg mb-6 ' >
             Add Task
           </button>
